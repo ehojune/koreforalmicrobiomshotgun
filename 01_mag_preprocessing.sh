2cc7886356ec18c58fb4f94cc665bf04b12d22c0
ehojune <ehojune@gmail.com>
2023-02-21 00:51:59 +0900

    실수로 git clean해서 중간파일들 좀 날림..

--- 01_mag_preprocessing.sh
+++ 01_mag_preprocessing.sh
@@ -6,7 +6,7 @@
 rawdata="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/raw_data/"
 scripts="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/scripts/"
 output="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/processed_data"
-hg38="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/GRCh38_noalt_as"
+hg38="GRCh38_noalt_as"
 
 samples="KorefMicrobiomeShotgun1 KorefMicrobiomeShotgun2"
 sample1="KorefMicrobiomeShotgun1"
@@ -18,8 +18,9 @@ fastp="/BiO/Access/ehojune/anaconda3/bin/fastp"
 megahit="/BiO/Access/ehojune/anaconda3/bin/megahit"
 spades="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/SPAdes-3.15.5-Linux/bin/metaspades.py"
 quast="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/quast/quast.py"
-bowtie2="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/bowtie2-2.5.1-mingw-x86_64/bowtie2"
+bowtie2="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/bowtie2-2.5.1/bowtie2"
 samtools="/BiO/Access/ehojune/anaconda3/bin/samtools"
+bedtools="/BiO/Access/ehojune/anaconda3/bin/bedtools"
 
 
 ### commands ###
@@ -40,7 +41,7 @@ do
 done
 echo "done"
 
-END
+
 #2. fastp after setting trimming criteria
 
 output_1=${output}/1_fastp_afterTrimming/
@@ -58,7 +59,7 @@ echo "start fastp after"
 
 
 touch ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh
-echo $fastp -i ${rawdata}${sample1}/${sample1}_1.fq.gz -I ${rawdata}${sample1}/${sample1}_2.fq.gz -o ${output_1}${sample1}/${sample1}_1.fq.gz -O ${output_1}${sample1}/${sample1}_2.fq.gz --html ${output_1}${sample1}/${sample1}.html --json ${output_1}${sample1}/${sample1}.json --report_title ${sample1}_after  --overrepresentation_analysis --trim_front1 18 --trim_tail1 3 --trim_front2 18 --trim_tail2 3 --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 0 --thread 16 --cut_front --average_qual 20 > ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh 
+echo $fastp -i ${rawdata}${sample1}/${sample1}_1.fq.gz -I ${rawdata}${sample1}/${sample1}_2.fq.gz -o ${output_1}${sample1}/${sample1}_1.fq.gz -O ${output_1}${sample1}/${sample1}_2.fq.gz --html ${output_1}${sample1}/${sample1}.html --json ${output_1}${sample1}/${sample1}.json --report_title ${sample1}_after  --overrepresentation_analysis --trim_front1 18 --trim_tail1 5 --trim_front2 18 --trim_tail2 3 --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 0 --thread 16 --cut_front --average_qual 20 > ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh 
 qsub -pe smp 16 -cwd ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh
 
 
@@ -66,11 +67,13 @@ qsub -pe smp 16 -cwd ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh
 
 
 
-:<<'END'
+
 
 touch ${scripts}KorefMicrobiomeShotgun2_fastp_after.sh
 echo $fastp -i ${rawdata}${sample2}/${sample2}_1.fq.gz -I ${rawdata}${sample2}/${sample2}_2.fq.gz -o ${output_1}${sample2}/${sample2}_1.fq.gz -O ${output_1}${sample2}/${sample2}_2.fq.gz --html ${output_1}${sample2}/${sample2}.html --json ${output_1}${sample2}/${sample2}.json --report_title ${sample2}_after  --overrepresentation_analysis --trim_front1 10 --trim_front2 10 --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 0 --thread 16 --cut_front --average_qual 20 > ${scripts}KorefMicrobiomeShotgun2_fastp_after.sh
 qsub -pe smp 16 -cwd ${scripts}KorefMicrobiomeShotgun2_fastp_after.sh
+
+
 
 
 rm -rf ${output_1}KorefMicrobiomeShotgun3
@@ -78,16 +81,23 @@ mkdir -p ${output_1}KorefMicrobiomeShotgun3
 cat ${output_1}${sample1}/${sample1}_1.fq.gz ${output_1}${sample2}/${sample2}_1.fq.gz > ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_1.fq.gz
 cat ${output_1}${sample1}/${sample1}_2.fq.gz ${output_1}${sample2}/${sample2}_2.fq.gz > ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_2.fq.gz
 
+
+
+
+
+output_1=${output}/1_fastp_afterTrimming/
+
 touch ${scripts}KorefMicrobiomeShotgun3_fastp.sh
 echo $fastp -i ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_1.fq.gz -I ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_2.fq.gz -o ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_1.fq.gz -O ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_2.fq.gz --html ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3.html --json ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3.json --report_title KorefMicrobiomeShotgun3_after  --overrepresentation_analysis --thread 16 > ${scripts}KorefMicrobiomeShotgun3_fastp_after.sh
 qsub -pe smp 16 -cwd ${scripts}KorefMicrobiomeShotgun3_fastp_after.sh
 
-echo "done"
 
+END
 
 #3. bowtie2 host removal
 
 samples="KorefMicrobiomeShotgun1 KorefMicrobiomeShotgun2 KorefMicrobiomeShotgun3"
+output_1=${output}/1_fastp_afterTrimming/
 output_2=${output}/2_bowtie/
 mkdir -p $output_2
 
@@ -96,14 +106,21 @@ for sample in $samples
 do
   mkdir -p ${output_2}${sample}
   touch  ${scripts}${sample}_bowtie.sh
-  echo $bowtie2 -p 64 -1 ${output_1}${sample}/${sample}_1.fq.gz -2 ${output_1}${sample}/${sample}_2.fq.gz -x $hg38 --un-conc-gz ${output_2}${sample}/${sample}_mapped_and_unmapped.sam > ${scripts}${sample}_bowtie.sh
+  echo $bowtie2 -p 64 -1 ${output_1}${sample}/${sample}_1.fq.gz -2 ${output_1}${sample}/${sample}_2.fq.gz -x $hg38 --un-conc-gz ${sample}.host_removed -S ${output_2}${sample}/${sample}_mapped_and_unmapped.sam > ${scripts}${sample}_bowtie.sh
+  echo mv ${sample}.host_removed.1 ${output_2}${sample} >> ${scripts}${sample}_bowtie.sh
+  echo mv ${sample}.host_removed.2 ${output_2}${sample} >> ${scripts}${sample}_bowtie.sh
+  echo mv ${output_2}${sample}/${sample}.host_removed.1 ${output_2}${sample}/${sample}.host_removed_R1.fastq.gz >> ${scripts}${sample}_bowtie.sh
+  echo mv ${output_2}${sample}/${sample}.host_removed.2 ${output_2}${sample}/${sample}.host_removed_R2.fastq.gz >> ${scripts}${sample}_bowtie.sh 
+  #echo $samtools view -bS ${output_2}${sample}/${sample}_mapped_and_unmapped.sam \> ${output_2}${sample}/${sample}_mapped_and_unmapped.bam >> ${scripts}${sample}_bowtie.sh
+  #echo $samtools view -b -f 12 -F 256 ${output_2}${sample}/${sample}_mapped_and_unmapped.bam \> ${output_2}${sample}/${sample}_bothEndsUnmapped.bam >> ${scripts}${sample}_bowtie.sh
+  #echo $samtools sort -n ${output_2}${sample}/${sample}_bothEndsUnmapped.bam ${output_2}${sample}/${sample}_bothEndsUnmapped_sorted.bam >> ${scripts}${sample}_bowtie.sh
+  #echo $bedtools bamtofastq -i ${output_2}${sample}/${sample}_bothEndsUnmapped_sorted.bam -fq ${output_2}${sample}/${sample}_host_removed_r1.fq -fq2 ${output_2}${sample}/${sample}_host_removed_r2.fq >> ${scripts}${sample}_bowtie.sh
   qsub -pe smp 64 -cwd ${scripts}${sample}_bowtie.sh
-  #$samtools view -bS ${output_2}${sample}/${sample}_mapped_and_unmapped.sam > ${output_2}${sample}/${sample}_mapped_and_unmapped.bam
+
 done
 echo "done"
 
 
-END
 
 
 
