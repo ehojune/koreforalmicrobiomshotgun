aa35df430b174f0921e13c3f07c121f95b86186a
ehojune <ehojune@gmail.com>
2023-02-20 10:56:23 +0900

    fastp after doing

--- 01_mag_preprocessing.sh
+++ 01_mag_preprocessing.sh
@@ -4,6 +4,7 @@
 ### paths ###
 
 rawdata="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/raw_data/"
+scripts="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/scripts/"
 output="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/processed_data"
 hg38="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/GRCh38_noalt_as"
 
@@ -52,15 +53,34 @@ done
 
 echo "start fastp after"
 
-echo $fastp -i ${rawdata}${sample1}/${sample1}_1.fq.gz -I ${rawdata}${sample1}/${sample1}_2.fq.gz -o ${output_1}${sample1}/${sample1}_1.fq.gz -O ${output_1}${sample1}/${sample1}_2.fq.gz --html ${output_1}${sample1}/${sample1}.html --json ${output_1}${sample1}/${sample1}.json --report_title ${sample1}_after  --overrepresentation_analysis --trim_front1 15 --trim_front2 15 --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 0 --thread 16 --cut_front --average_qual 20
 
 
-echo $fastp -i ${rawdata}${sample2}/${sample2}_1.fq.gz -I ${rawdata}${sample2}/${sample2}_2.fq.gz -o ${output_1}${sample2}/${sample2}_1.fq.gz -O ${output_1}${sample2}/${sample2}_2.fq.gz --html ${output_1}${sample2}/${sample2}.html --json ${output_1}${sample2}/${sample2}.json --report_title ${sample2}_after  --overrepresentation_analysis --trim_front1 10 --trim_front2 10 --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 0 --thread 16 --cut_front --average_qual 20
 
+
+touch ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh
+echo $fastp -i ${rawdata}${sample1}/${sample1}_1.fq.gz -I ${rawdata}${sample1}/${sample1}_2.fq.gz -o ${output_1}${sample1}/${sample1}_1.fq.gz -O ${output_1}${sample1}/${sample1}_2.fq.gz --html ${output_1}${sample1}/${sample1}.html --json ${output_1}${sample1}/${sample1}.json --report_title ${sample1}_after  --overrepresentation_analysis --trim_front1 18 --trim_tail1 3 --trim_front2 18 --trim_tail2 3 --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 0 --thread 16 --cut_front --average_qual 20 > ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh 
+qsub -pe smp 16 -cwd ${scripts}KorefMicrobiomeShotgun1_fastp_after.sh
+
+
+
+
+
+
+:<<'END'
+
+touch ${scripts}KorefMicrobiomeShotgun2_fastp_after.sh
+echo $fastp -i ${rawdata}${sample2}/${sample2}_1.fq.gz -I ${rawdata}${sample2}/${sample2}_2.fq.gz -o ${output_1}${sample2}/${sample2}_1.fq.gz -O ${output_1}${sample2}/${sample2}_2.fq.gz --html ${output_1}${sample2}/${sample2}.html --json ${output_1}${sample2}/${sample2}.json --report_title ${sample2}_after  --overrepresentation_analysis --trim_front1 10 --trim_front2 10 --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 0 --thread 16 --cut_front --average_qual 20 > ${scripts}KorefMicrobiomeShotgun2_fastp_after.sh
+qsub -pe smp 16 -cwd ${scripts}KorefMicrobiomeShotgun2_fastp_after.sh
+
+
+rm -rf ${output_1}KorefMicrobiomeShotgun3
 mkdir -p ${output_1}KorefMicrobiomeShotgun3
 cat ${output_1}${sample1}/${sample1}_1.fq.gz ${output_1}${sample2}/${sample2}_1.fq.gz > ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_1.fq.gz
 cat ${output_1}${sample1}/${sample1}_2.fq.gz ${output_1}${sample2}/${sample2}_2.fq.gz > ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_2.fq.gz
-$fastp -i ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_1.fq.gz -I ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_2.fq.gz -o ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_1.fq.gz -O ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_2.fq.gz --html ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3.html --json ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3.json --report_title KorefMicrobiomeShotgun3_after  --overrepresentation_analysis --thread 16
+
+touch ${scripts}KorefMicrobiomeShotgun3_fastp.sh
+echo $fastp -i ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_1.fq.gz -I ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_catted_2.fq.gz -o ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_1.fq.gz -O ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3_2.fq.gz --html ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3.html --json ${output_1}KorefMicrobiomeShotgun3/KorefMicrobiomeShotgun3.json --report_title KorefMicrobiomeShotgun3_after  --overrepresentation_analysis --thread 16 > ${scripts}KorefMicrobiomeShotgun3_fastp_after.sh
+qsub -pe smp 16 -cwd ${scripts}KorefMicrobiomeShotgun3_fastp_after.sh
 
 echo "done"
 
@@ -70,19 +90,20 @@ echo "done"
 samples="KorefMicrobiomeShotgun1 KorefMicrobiomeShotgun2 KorefMicrobiomeShotgun3"
 output_2=${output}/2_bowtie/
 mkdir -p $output_2
-unzip $hg38.zip
 
-echo "start fastp before"
+echo "start bowtie"
 for sample in $samples
 do
   mkdir -p ${output_2}${sample}
-  $bowtie2 -p 64 -1 ${output_1}${sample}/${sample}_1.fq.gz -2 ${output_1}${sample}/${sample}_2.fq.gz -x $hg38 --un-conc-gz ${output_2}${sample}/${sample}_mapped_and_unmapped.sam
-  $samtools view -bS ${output_2}${sample}/${sample}_mapped_and_unmapped.sam > ${output_2}${sample}/${sample}_mapped_and_unmapped.bam
+  touch  ${scripts}${sample}_bowtie.sh
+  echo $bowtie2 -p 64 -1 ${output_1}${sample}/${sample}_1.fq.gz -2 ${output_1}${sample}/${sample}_2.fq.gz -x $hg38 --un-conc-gz ${output_2}${sample}/${sample}_mapped_and_unmapped.sam > ${scripts}${sample}_bowtie.sh
+  qsub -pe smp 64 -cwd ${scripts}${sample}_bowtie.sh
+  #$samtools view -bS ${output_2}${sample}/${sample}_mapped_and_unmapped.sam > ${output_2}${sample}/${sample}_mapped_and_unmapped.bam
 done
 echo "done"
 
 
-
+END
 
 
 
