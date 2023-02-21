18fe9bfc8ec447ab01c5aa6677d1ce3e5c2007f9
ehojune <ehojune@gmail.com>
2023-02-21 12:57:14 +0900

    file modification after bowtie done

--- 3_mag_assembly.sh
+++ 3_mag_assembly.sh
@@ -17,7 +17,8 @@ sample3="KorefMicrobiomeShotgun3"
 
 fastp="/BiO/Access/ehojune/anaconda3/bin/fastp"
 megahit="/BiO/Access/ehojune/anaconda3/bin/megahit"
-spades="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/SPAdes-3.15.5-Linux/bin/metaspades.py"
+spades="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/SPAdes-3.15.2-Linux/bin/metaspades.py"
+
 quast="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/quast/quast.py"
 bowtie2="/BiO/Research/Project1/KOREF_PersonalMultiomicsReference/Workspace/ehojune/tools/bowtie2-2.5.1/bowtie2"
 samtools="/BiO/Access/ehojune/anaconda3/bin/samtools"
@@ -32,7 +33,7 @@ python="/BiO/Access/ehojune/anaconda3/bin/python"
 #3_1. megahit
 
 
-output_2=${output}2_bowtie/
+output_2=${output}2_bowtie_modified/
 output_3_1=${output}3_1_megahit/
 
 echo "start megahit"
@@ -43,7 +44,7 @@ do
   echo export PATH=${python}:\$PATH > ${scripts}${sample}_megahit.sh
   echo export PYTHONPATH=\$PYTHONPATH:${python} >> ${scripts}${sample}_megahit.sh
   echo $megahit -1 ${output_2}${sample}/${sample}_host_removed_r1.fq.gz -2 ${output_2}${sample}/${sample}_host_removed_r2.fq.gz -o ${output_3_1}${sample}/${sample}.megahit_asm --memory 0.1 -t 60 >> ${scripts}${sample}_megahit.sh
-  #qsub -cwd -pe smp 60 ${scripts}${sample}_megahit.sh
+  qsub -cwd -pe smp 60 ${scripts}${sample}_megahit.sh
 done
 echo "done"
 
@@ -53,7 +54,7 @@ echo "done"
 #3_2. metaspades
 
 
-output_2=${output}2_bowtie/
+output_2=${output}2_bowtie_modified/
 output_3_2=${output}3_2_metaspades/
 mkdir ${output_3_2}
 
@@ -65,7 +66,7 @@ do
   echo export PATH=${python}:\$PATH > ${scripts}${sample}_metaspades.sh
   echo export PYTHONPATH=\$PYTHONPATH:${python} >> ${scripts}${sample}_metaspades.sh
   echo $python $spades --meta -1 ${output_2}${sample}/${sample}_host_removed_r1.fq.gz -2 ${output_2}${sample}/${sample}_host_removed_r2.fq.gz -o ${output_3_2}${sample}/${sample}.metaspades_asm -t 100 --memory 3000 >> ${scripts}${sample}_metaspades.sh
-  qsub -cwd -pe smp 100 ${scripts}${sample}_metaspades.sh
+  #qsub -cwd -pe smp 100 ${scripts}${sample}_metaspades.sh
 done
 echo "done"
 
